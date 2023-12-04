d1e7e5594f2eae80d5e1a24bd1345ff4da048dab
David Whetstone <david@humblehacker.com>
2023-12-04 11:25:21 -0800

    day04: setup

---
+++ Sources/Day04.swift
@@ -0,0 +1,15 @@
+import Algorithms
+import Foundation
+import Parsing
+
+struct Day04: AdventDay {
+    var data: String
+
+    func part1() -> Int {
+        return 0
+    }
+
+    func part2() -> Int {
+        return 0
+    }
+}
