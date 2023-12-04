0e72df680591559bbdbc5c1e757399c4c5313780
David Whetstone <david@humblehacker.com>
2023-12-04 15:28:58 -0800

    day04: part 1

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -6,10 +6,84 @@ struct Day04: AdventDay {
     var data: String
 
     func part1() -> Int {
-        return 0
+        let cards = try! CardsParser().parse(data)
+        let points = cards.map { Day04.calculatePoints(card: $0) }
+        return points.reduce(0, +)
     }
 
     func part2() -> Int {
         return 0
+    }
+
+    static func parseCard(input: String) throws -> Card {
+        try CardParser().parse(input)
+    }
+
+    static func parseCards(input: String) throws -> [Card] {
+        try CardsParser().parse(input)
+    }
+
+    static func calculatePoints(card: Card) -> Int {
+        let matchingNumbers = Set(card.winningNumbers).intersection(card.ourNumbers)
+        return Int(pow(Double(2), Double(matchingNumbers.count - 1)))
+    }
+}
+
+struct Card: Equatable {
+    let id: Int
+    let winningNumbers: [Int]
+    let ourNumbers: [Int]
+}
+
+struct CardParser: Parser {
+    var body: some Parser<Substring, Card> {
+        Parse {
+            Card(id: $0, winningNumbers: $1, ourNumbers: $2)
+        } with: {
+            // id
+            "Card".utf8
+            Whitespace()
+            Int.parser()
+            ": ".utf8
+
+            // winning numbers
+            Many {
+                OneOf {
+                    Digits(2)
+                    Parse {
+                        " ".utf8
+                        Digits(1)
+                    }
+                }
+            } separator: {
+                " ".utf8
+            }
+
+            " | ".utf8
+
+            // our numbers
+            Many {
+                OneOf {
+                    Digits(2)
+                    Parse {
+                        " ".utf8
+                        Digits(1)
+                    }
+                }
+            } separator: {
+                " ".utf8
+            }
+        }
+    }
+}
+
+struct CardsParser: Parser {
+    var body: some Parser<Substring, [Card]> {
+        Many {
+            CardParser()
+        } separator: {
+            "\n"
+        }
+        Skip { Optionally { "\n" } }
     }
 }
--- Tests/Day04.swift
+++ Tests/Day04.swift
@@ -13,6 +13,40 @@ final class Day04Tests: XCTestCase {
     Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     """
 
+    func testParseCard() throws {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+        let actual = try Day04.parseCard(input: input)
+        let expected = Card(id: 1, winningNumbers: [41, 48, 83, 86, 17], ourNumbers: [83, 86, 6, 31, 17, 9, 48, 53])
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testParseCards() throws {
+        let actual = try Day04.parseCards(input: testData)
+        let expected = [
+            Card(id: 1, winningNumbers: [41, 48, 83, 86, 17], ourNumbers: [83, 86, 6, 31, 17, 9, 48, 53]),
+            Card(id: 2, winningNumbers: [13, 32, 20, 16, 61], ourNumbers: [61, 30, 68, 82, 17, 32, 24, 19]),
+            Card(id: 3, winningNumbers: [1, 21, 53, 59, 44], ourNumbers: [69, 82, 63, 72, 16, 21, 14, 1]),
+            Card(id: 4, winningNumbers: [41, 92, 73, 84, 69], ourNumbers: [59, 84, 76, 51, 58, 5, 54, 83]),
+            Card(id: 5, winningNumbers: [87, 83, 26, 28, 32], ourNumbers: [88, 30, 70, 12, 93, 22, 82, 36]),
+            Card(id: 6, winningNumbers: [31, 18, 13, 56, 72], ourNumbers: [74, 77, 10, 23, 35, 67, 36, 11]),
+        ]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testCalculatePoints_withWins() throws {
+        let card = Card(id: 1, winningNumbers: [41, 48, 83, 86, 17], ourNumbers: [83, 86, 6, 31, 17, 9, 48, 53])
+        let actual = Day04.calculatePoints(card: card)
+        let expected = 8
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testCalculatePoints_noWins() throws {
+        let card = Card(id: 6, winningNumbers: [31, 18, 13, 56, 72], ourNumbers: [74, 77, 10, 23, 35, 67, 36, 11])
+        let actual = Day04.calculatePoints(card: card)
+        let expected = 0
+        XCTAssertNoDifference(expected, actual)
+    }
+
     func testPart1() throws {
         let challenge = Day04(data: testData)
         XCTAssertNoDifference(13, challenge.part1())
