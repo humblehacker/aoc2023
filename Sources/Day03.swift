01475e37d8a963694a8750764a91db1f8b2de7cd
David Whetstone <david@humblehacker.com>
2023-12-03 20:04:45 -0800

    day03: part 1

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -9,6 +9,98 @@ struct Day03: AdventDay {
         data.split(separator: "\n").map { String($0) }
     }
 
+    var partNumbers: [PartNumber] {
+        Self.parsePartNumbers(input: entities)
+    }
+
+    var symbols: [Symbol] {
+        Self.parseSymbols(input: entities)
+    }
+
+    static let symbolChars = "#$%&*+-/=@"
+
+    public static func parseSymbols(input: [String]) -> [Symbol] {
+        var result: [Symbol] = []
+        var row = 0
+        for line in input {
+            var column = 0
+            for c in line {
+                if symbolChars.contains(c) {
+                    result.append(Symbol(value: String(c), coordinate: Coordinate(x: column, y: row)))
+                }
+                column += 1
+            }
+            row += 1
+        }
+
+        return result
+    }
+
+    static func parsePartNumbers(input: [String]) -> [PartNumber] {
+        var result: [PartNumber] = []
+        var row = 0
+        for line in input {
+            var column = 0
+            var number = ""
+            var coordinate: Coordinate?
+            for c in line {
+                if c.isNumber {
+                    if number.isEmpty {
+                        coordinate = Coordinate(x: column, y: row)
+                    }
+                    number.append(String(c))
+                } else if let coord = coordinate {
+                    result.append(PartNumber(number: Int(number)!, coordinate: coord))
+                    number = ""
+                    coordinate = nil
+                }
+
+                column += 1
+            }
+            // handle if last character in row is a digit
+            if let coord = coordinate {
+                result.append(PartNumber(Int(number)!, coordinate: coord))
+                number = ""
+                coordinate = nil
+            }
+            row += 1
+        }
+
+        return result
+    }
+
+    typealias SymbolMap = [Int: [Int: Symbol]]
+
+    static func generateSymbolMap(_ symbols: [Symbol]) -> SymbolMap {
+        var result: SymbolMap = [:]
+
+        for symbol in symbols {
+            result[symbol.y, default: .init()][symbol.x] = symbol
+        }
+
+        return result
+    }
+
+    static func coordinates(for partNumber: PartNumber, maxX: Int, maxY: Int) -> [Coordinate] {
+        let range = partNumber.x ... partNumber.x + String(partNumber.number).count - 1
+        let yRange = max(0, partNumber.y - 1) ... min(maxY, partNumber.y + 1)
+        let xRange = max(0, range.first! - 1) ... min(maxX, range.last! + 1)
+        var result: [Coordinate] = []
+        for y in yRange {
+            for x in xRange {
+                guard !range.contains(x) || y != partNumber.y else { continue }
+                result.append(Coordinate(x: x, y: y))
+            }
+        }
+
+        return result
+    }
+
+    static func symbols(adjacentTo coordinates: [Coordinate], in symbolMap: SymbolMap) -> [Symbol] {
+        let adjacentSymbols = coordinates.compactMap { coordinate in symbolMap[coordinate.y]?[coordinate.x] }
+        return adjacentSymbols
+    }
+
     // sum all numbers adjacent to a symbol
     //
     //       a b c
@@ -16,10 +108,51 @@ struct Day03: AdventDay {
     //       f g h
     //
     func part1() -> Int {
-        return 0
+        let entities = data.split(separator: "\n").map { String($0) }
+        let partNumbers = Self.parsePartNumbers(input: entities)
+        let symbols = Self.parseSymbols(input: entities)
+        let symbolMap = Self.generateSymbolMap(symbols)
+
+        var adjacentNumbers: [PartNumber] = []
+        for number in partNumbers {
+            let coordinates = Self.coordinates(for: number, maxX: entities.first!.count, maxY: entities.count)
+            let symbols = Self.symbols(adjacentTo: coordinates, in: symbolMap)
+            for _ in symbols {
+                adjacentNumbers.append(number)
+            }
+        }
+
+        let sum = adjacentNumbers.map { $0.number }.reduce(0, +)
+        return sum
     }
 
     func part2() -> Int {
         return 0
+    }
+}
+
+struct Coordinate: Equatable {
+    let x: Int
+    let y: Int
+}
+
+struct Symbol: Equatable {
+    let value: String
+    let coordinate: Coordinate
+
+    var x: Int { coordinate.x }
+    var y: Int { coordinate.y }
+}
+
+struct PartNumber: Equatable {
+    let number: Int
+    let coordinate: Coordinate
+
+    var x: Int { coordinate.x }
+    var y: Int { coordinate.y }
+
+    init(_ number: Int, coordinate: Coordinate) {
+        self.number = number
+        self.coordinate = coordinate
     }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -7,7 +7,7 @@ final class Day03Tests: XCTestCase {
     let testData = """
     467..114..
     ...*......
-    ..35..633.
+    ..35...633
     ......#...
     617*......
     .....+.58.
@@ -16,6 +16,92 @@ final class Day03Tests: XCTestCase {
     ...$.*....
     .664.598..
     """
+
+    func testParseSymbols() throws {
+        let actual = Day03.parseSymbols(input: testData.split(separator: "\n").map { String($0) })
+        let expected = [
+            Symbol(value: "*", coordinate: Coordinate(x: 3, y: 1)),
+            Symbol(value: "#", coordinate: Coordinate(x: 6, y: 3)),
+            Symbol(value: "*", coordinate: Coordinate(x: 3, y: 4)),
+            Symbol(value: "+", coordinate: Coordinate(x: 5, y: 5)),
+            Symbol(value: "$", coordinate: Coordinate(x: 3, y: 8)),
+            Symbol(value: "*", coordinate: Coordinate(x: 5, y: 8)),
+        ]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testParsePartNumbers() throws {
+        let actual = Day03.parsePartNumbers(input: testData.split(separator: "\n").map { String($0) })
+        let expected = [
+            PartNumber(467, coordinate: Coordinate(x: 0, y: 0)),
+            PartNumber(114, coordinate: Coordinate(x: 5, y: 0)),
+            PartNumber(35, coordinate: Coordinate(x: 2, y: 2)),
+            PartNumber(633, coordinate: Coordinate(x: 7, y: 2)),
+            PartNumber(617, coordinate: Coordinate(x: 0, y: 4)),
+            PartNumber(58, coordinate: Coordinate(x: 7, y: 5)),
+            PartNumber(592, coordinate: Coordinate(x: 2, y: 6)),
+            PartNumber(755, coordinate: Coordinate(x: 6, y: 7)),
+            PartNumber(664, coordinate: Coordinate(x: 1, y: 9)),
+            PartNumber(598, coordinate: Coordinate(x: 5, y: 9)),
+        ]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testGenerateSymbolMap() throws {
+        let symbols = Day03.parseSymbols(input: testData.split(separator: "\n").map { String($0) })
+        let actual = Day03.generateSymbolMap(symbols)
+        let expected = [
+            1: [3: Symbol(value: "*", coordinate: Coordinate(x: 3, y: 1))],
+            3: [6: Symbol(value: "#", coordinate: Coordinate(x: 6, y: 3))],
+            4: [3: Symbol(value: "*", coordinate: Coordinate(x: 3, y: 4))],
+            5: [5: Symbol(value: "+", coordinate: Coordinate(x: 5, y: 5))],
+            8: [
+                3: Symbol(value: "$", coordinate: Coordinate(x: 3, y: 8)),
+                5: Symbol(value: "*", coordinate: Coordinate(x: 5, y: 8)),
+            ],
+        ]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testCoordinatesForPartNumber() throws {
+        do {
+            let actual = Day03.coordinates(for: PartNumber(35, coordinate: Coordinate(x: 2, y: 2)), maxX: 9, maxY: 9)
+            let expected = [
+                Coordinate(x: 1, y: 1),
+                Coordinate(x: 2, y: 1),
+                Coordinate(x: 3, y: 1),
+                Coordinate(x: 4, y: 1),
+                Coordinate(x: 1, y: 2),
+                Coordinate(x: 4, y: 2),
+                Coordinate(x: 1, y: 3),
+                Coordinate(x: 2, y: 3),
+                Coordinate(x: 3, y: 3),
+                Coordinate(x: 4, y: 3),
+            ]
+            XCTAssertNoDifference(expected, actual)
+        }
+        do {
+            /*    6 7 8 9
+               1  . . . .
+               2  . 6 3 3
+               3  # . . .
+
+             */
+            let actual = Day03.coordinates(for: PartNumber(633, coordinate: Coordinate(x: 7, y: 2)), maxX: 9, maxY: 9)
+            let expected = [
+                Coordinate(x: 6, y: 1),
+                Coordinate(x: 7, y: 1),
+                Coordinate(x: 8, y: 1),
+                Coordinate(x: 9, y: 1),
+                Coordinate(x: 6, y: 2),
+                Coordinate(x: 6, y: 3),
+                Coordinate(x: 7, y: 3),
+                Coordinate(x: 8, y: 3),
+                Coordinate(x: 9, y: 3),
+            ]
+            XCTAssertNoDifference(expected, actual)
+        }
+    }
 
     func testPart1() throws {
         let challenge = Day03(data: testData)
