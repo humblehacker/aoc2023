f3920b16fb7cd48e48e9a6c623bec8896c16cd13
David Whetstone <david@humblehacker.com>
2023-12-06 17:42:31 -0800

    day 05: part 1

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -4,12 +4,168 @@ import Parsing
 
 struct Day05: AdventDay {
     var data: String
+    let almanac: Almanac
 
-    func part1() -> Int {
-        return 0
+    init(data: String) {
+        self.data = data
+        almanac = try! AlmanacParser().parse(data)
     }
 
     func part2() -> Int {
         return 0
+    func part1() throws -> Int {
+        var locations: [Int] = []
+        for seed in almanac.seeds {
+            let location = almanac.seedLocation(seed)
+            locations.append(location)
+        }
+
+        return locations.sorted().first ?? 0
+    }
+}
+
+struct Almanac: Equatable {
+    let seeds: [Int]
+    let maps: [Map]
+}
+
+extension Almanac {
+    func seedLocation(_ seed: Int) -> Int {
+        let soil = seedToSoil(seed)
+        let fertilizer = soilToFertilizer(soil)
+        let water = fertilizerToWater(fertilizer)
+        let light = waterToLight(water)
+        let temperature = lightToTemperature(light)
+        let humidity = temperatureToHumidity(temperature)
+        let location = humidityToLocation(humidity)
+        return location
+    }
+
+    func seedToSoil(_ seed: Int) -> Int {
+        return destinationFromSource(map: "seed-to-soil", source: seed)
+    }
+
+    func soilToFertilizer(_ soil: Int) -> Int {
+        return destinationFromSource(map: "soil-to-fertilizer", source: soil)
+    }
+
+    func fertilizerToWater(_ fertilizer: Int) -> Int {
+        return destinationFromSource(map: "fertilizer-to-water", source: fertilizer)
+    }
+
+    func waterToLight(_ water: Int) -> Int {
+        return destinationFromSource(map: "water-to-light", source: water)
+    }
+
+    func lightToTemperature(_ light: Int) -> Int {
+        return destinationFromSource(map: "light-to-temperature", source: light)
+    }
+
+    func temperatureToHumidity(_ temperature: Int) -> Int {
+        return destinationFromSource(map: "temperature-to-humidity", source: temperature)
+    }
+
+    func humidityToLocation(_ humidity: Int) -> Int {
+        return destinationFromSource(map: "humidity-to-location", source: humidity)
+    }
+
+    func destinationFromSource(map: String, source: Int) -> Int {
+        guard let map = maps.first(where: { $0.name == map }) else { return 0 }
+        return map.destinationFromSource(source)
+    }
+}
+
+struct Map: Equatable {
+    let name: String
+    let entries: [MapEntry]
+}
+
+extension Map {
+    func destinationFromSource(_ source: Int) -> Int {
+        for entry in entries {
+            if let dest = entry.destinationFromSource(source) {
+                return dest
+            }
+        }
+        return source
+    }
+}
+
+struct MapEntry: Equatable {
+    let dstRange: Range<Int>
+    let srcRange: Range<Int>
+}
+
+extension MapEntry {
+    func destinationFromSource(_ source: Int) -> Int? {
+        guard let index = srcRange.firstIndex(of: source) else { return nil }
+        let offset = index - srcRange.startIndex
+        return dstRange[dstRange.index(dstRange.startIndex, offsetBy: offset)]
+    }
+}
+
+extension MapEntry {
+    init(dstRangeStart: Int, srcRangeStart: Int, rangeLength: Int) {
+        dstRange = dstRangeStart ..< dstRangeStart + rangeLength
+        srcRange = srcRangeStart ..< srcRangeStart + rangeLength
+    }
+}
+
+// MARK: Parsers
+
+struct AlmanacParser: Parser {
+    var body: some Parser<Substring, Almanac> {
+        Parse(Almanac.init(seeds:maps:)) {
+            SeedsParser()
+            "\n\n".utf8
+            Many {
+                MapParser()
+            } separator: {
+                "\n\n"
+            } terminator: {
+                ""
+            }
+            Skip { Optionally { "\n" }}
+        }
+    }
+}
+
+struct SeedsParser: Parser {
+    var body: some Parser<Substring, [Int]> {
+        "seeds: "
+        Many {
+            Int.parser()
+        } separator: {
+            " ".utf8
+        }
+    }
+}
+
+struct MapParser: Parser {
+    var body: some Parser<Substring, Map> {
+        Parse(Map.init(name:entries:)) {
+            Prefix { $0 != " " }.map(String.init)
+            " map:".utf8
+            "\n".utf8
+            Many {
+                MapEntryParser()
+            } separator: {
+                "\n"
+            } terminator: {
+                ""
+            }
+        }
+    }
+}
+
+struct MapEntryParser: Parser {
+    var body: some Parser<Substring, MapEntry> {
+        Parse(MapEntry.init(dstRangeStart:srcRangeStart:rangeLength:)) {
+            Int.parser()
+            " "
+            Int.parser()
+            " "
+            Int.parser()
+        }
     }
 }
--- Tests/Day05.swift
+++ Tests/Day05.swift
@@ -37,12 +37,71 @@ final class Day05Tests: XCTestCase {
 
     humidity-to-location map:
     60 56 37
-    56 93 4   
+    56 93 4
     """
+
+    func testParseMap() throws {
+        let input = """
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+        """
+        let expected = Map(
+            name: "fertilizer-to-water",
+            entries: [
+                MapEntry(dstRange: 49 ..< 49 + 8, srcRange: 53 ..< 53 + 8),
+                MapEntry(dstRange: 0 ..< 0 + 42, srcRange: 11 ..< 11 + 42),
+                MapEntry(dstRange: 42 ..< 42 + 7, srcRange: 0 ..< 0 + 7),
+                MapEntry(dstRange: 57 ..< 57 + 4, srcRange: 7 ..< 7 + 4),
+            ]
+        )
+        let actual = try MapParser().parse(input)
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testAlmanacParser() throws {
+        let input = """
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+        """
+
+        let expected = Almanac(
+            seeds: [79, 14, 55, 13],
+            maps: [
+                Map(
+                    name: "seed-to-soil",
+                    entries: [
+                        MapEntry(dstRange: 50 ..< 50 + 2, srcRange: 98 ..< 98 + 2),
+                        MapEntry(dstRange: 52 ..< 52 + 48, srcRange: 50 ..< 50 + 48),
+                    ]
+                ),
+                Map(
+                    name: "soil-to-fertilizer",
+                    entries: [
+                        MapEntry(dstRange: 0 ..< 0 + 37, srcRange: 15 ..< 15 + 37),
+                        MapEntry(dstRange: 37 ..< 37 + 2, srcRange: 52 ..< 52 + 2),
+                        MapEntry(dstRange: 39 ..< 39 + 15, srcRange: 0 ..< 0 + 15),
+                    ]
+                ),
+            ]
+        )
+        let actual = try AlmanacParser().parse(input)
+        XCTAssertNoDifference(expected, actual)
+    }
 
     func testPart1() throws {
         let challenge = Day05(data: testData)
-        XCTAssertNoDifference(35, challenge.part1())
+        XCTAssertNoDifference(35, try challenge.part1())
     }
 
     func testPart2() throws {
