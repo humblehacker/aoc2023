e251d6d1fc1c6aff6281c2f2416ba83f5ea04e07
David Whetstone <david@humblehacker.com>
2023-12-08 16:38:17 -0800

    Day06: Part 1

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -1,4 +1,5 @@
 import Algorithms
+import CustomDump
 import Foundation
 import Parsing
 
@@ -6,10 +7,61 @@ struct Day06: AdventDay {
     var data: String
 
     func part1() async throws -> Int {
-        return 0
+        let stats = try RaceStatsParser().parse(data)
+        return stats.races
+            .map { $0.buttonPressesBeatingRecord() }
+            .map { $0.count }
+            .reduce(1, *)
     }
 
     func part2() async throws -> Int {
         return 0
+    }
+}
+
+struct RaceStats: Equatable {
+    let races: [Race]
+}
+
+struct Race: Equatable {
+    let time: Int
+    let recordDistance: Int
+}
+
+extension Race {
+    func distance(for buttonPressTime: Int) -> Int {
+        let remainingTime = time - buttonPressTime
+        return buttonPressTime * remainingTime
+    }
+
+    func buttonPressesBeatingRecord() -> [Int] {
+        (0 ..< time)
+            .map { distance(for: $0) }
+            .filter { $0 > recordDistance }
+    }
+}
+
+struct RaceStatsParser: Parser {
+    var body: some Parser<Substring, RaceStats> {
+        Parse { (times: [Int], distances: [Int]) in
+            RaceStats(races: zip(times, distances).map { Race(time: $0, recordDistance: $1) })
+        } with: {
+            "Time:".utf8
+            Whitespace()
+            Many {
+                Int.parser()
+            } separator: {
+                Whitespace()
+            }
+            "\n".utf8
+            "Distance:".utf8
+            Whitespace()
+            Many {
+                Int.parser()
+            } separator: {
+                Whitespace()
+            }
+            Skip { Optionally { "\n" } }
+        }
     }
 }
--- Tests/Day06.swift
+++ Tests/Day06.swift
@@ -5,11 +5,13 @@ import XCTest
 
 final class Day06Tests: XCTestCase {
     let testData = """
+    Time:      7  15   30
+    Distance:  9  40  200
     """
 
     func testPart1() async throws {
         let actual = try await Day06(data: testData).part1()
-        let expected = 0
+        let expected = 288
         XCTAssertNoDifference(expected, actual)
     }
 
