109056c3d8712625a10aaa4941776a5b1787f34f
David Whetstone <david@humblehacker.com>
2023-12-09 12:13:45 -0800

    day07: part 2

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -14,7 +14,10 @@ struct Day07: AdventDay {
     }
 
     func part2() async throws -> Int {
-        return 0
+        let game = try GameParser(jokersWild: true).parse(data)
+        let orderedHands = game.orderedHands()
+        let winnings = Game.winnings(orderedHands: orderedHands)
+        return winnings.reduce(0, +)
     }
 
     struct Game: Equatable {
@@ -46,48 +49,75 @@ struct Day07: AdventDay {
             case fiveOfKind
         }
 
-        static func kind(cards: [Card]) -> Kind {
+        static func kind(cards: [Card], jokersWild: Bool = false) -> Kind {
             precondition(cards.count == 5)
+            let cardStrengths = jokersWild ? Card.strengthsP2 : Card.strengthsP1
 
             // count each strength in hand
-            let counts = (0 ... Card.strengths.count).map { strength in
+            var counts = (0 ... cardStrengths.count).map { strength in
                 cards.filter { $0.strength == strength }.count
             }
 
-            if counts.contains(where: { $0 == 5 }) {
-                return .fiveOfKind
+            var jokersCount = 0
+            if jokersWild {
+                jokersCount = counts[0]
+                counts[0] = 0
             }
 
-            if counts.contains(where: { $0 == 4 }) {
-                return .fourOfKind
-            }
+            func count(_ count: Int, jokers: Int = 0) -> Int { counts.filter { $0 + jokers == count }.count }
 
-            switch counts.filter({ $0 == 2 }).count {
-            case 2: return .twoPair
-            case 1: return counts.contains(where: { $0 == 3 }) ? .fullHouse : .onePair
+            switch (count(2), count(3), count(4), count(5), jokersCount) {
+            case (0, 0, 0, 1, 0): return .fiveOfKind
+            case (0, 0, 1, 0, 1): return .fiveOfKind
+            case (0, 1, 0, 0, 2): return .fiveOfKind
+            case (1, 0, 0, 0, 3): return .fiveOfKind
+            case (0, 0, 0, 0, 4): return .fiveOfKind
+            case (0, 0, 0, 0, 5): return .fiveOfKind
+
+            case (0, 0, 1, 0, 0): return .fourOfKind
+            case (0, 1, 0, 0, 1): return .fourOfKind
+            case (1, 0, 0, 0, 2): return .fourOfKind
+            case (0, 0, 0, 0, 3): return .fourOfKind
+
+            case (1, 1, 0, 0, 0): return .fullHouse
+            case (2, 0, 0, 0, 1): return .fullHouse
+
+            case (0, 1, 0, 0, 0): return .threeOfKind
+            case (1, 0, 0, 0, 1): return .threeOfKind
+            case (0, 0, 0, 0, 2): return .threeOfKind
+
+            case (2, 0, 0, 0, 0): return .twoPair
+
+            case (1, 0, 0, 0, 0): return .onePair
+            case (0, 0, 0, 0, 1): return .onePair
+
+            case (0, 0, 0, 0, 0): return .highCard
             default: break
             }
 
-            return counts.contains(where: { $0 == 3 }) ? .threeOfKind : .highCard
+            return .highCard
         }
     }
 
     struct Card: Equatable {
         let value: String
         let strength: Int
-        static let strengths: [Character] = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+        static let strengthsP1: [Character] = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+        static let strengthsP2: [Character] = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
     }
 
     struct HandParser: Parser {
+        let jokersWild: Bool
         var body: some Parser<Substring, Hand> {
             Parse { (cardValues: String, bid: Int) -> Hand in
+                let cardStrengths = jokersWild ? Card.strengthsP2 : Card.strengthsP1
                 let cards = cardValues
-                    .map { Card(value: String($0), strength: Card.strengths.firstIndex(of: $0)!) }
+                    .map { Card(value: String($0), strength: cardStrengths.firstIndex(of: $0)!) }
 
                 return Hand(
                     cards: cards,
                     bid: bid,
-                    kind: Hand.kind(cards: cards)
+                    kind: Hand.kind(cards: cards, jokersWild: jokersWild)
                 )
             } with: {
                 Prefix(5).map(.string)
@@ -98,10 +128,16 @@ struct Day07: AdventDay {
     }
 
     struct GameParser: Parser {
+        let jokersWild: Bool
+
+        init(jokersWild: Bool = false) {
+            self.jokersWild = jokersWild
+        }
+
         var body: some Parser<Substring, Game> {
             Parse(Game.init) {
                 Many {
-                    HandParser()
+                    HandParser(jokersWild: jokersWild)
                 } separator: {
                     "\n"
                 }
--- Tests/Day07.swift
+++ Tests/Day07.swift
@@ -12,7 +12,7 @@ final class Day07Tests: XCTestCase {
     QQQJA 483
     """
 
-    func testCardKind() throws {
+    func testCardKindNoJokers() throws {
         let game = try Day07.GameParser().parse("""
         AAAAA 0
         AAAA2 0
@@ -35,10 +35,116 @@ final class Day07Tests: XCTestCase {
         XCTAssertNoDifference(expected, actual)
     }
 
+    func testFiveOfKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        AAAAA 0
+        JAAAA 0
+        JJAAA 0
+        JJJAA 0
+        JJJJA 0
+        JJJJJ 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [.fiveOfKind, .fiveOfKind, .fiveOfKind, .fiveOfKind, .fiveOfKind, .fiveOfKind]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testFourOfKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        AAAA2 0
+        JAAA2 0
+        JJAA2 0
+        JJJA2 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [.fourOfKind, .fourOfKind, .fourOfKind, .fourOfKind]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testFullHouseKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        AAA22 0
+        JAA22 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [
+            .fullHouse,
+            .fullHouse,
+        ]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testThreeKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        AAA23 0
+        JAA23 0
+        JJA23 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [.threeOfKind, .threeOfKind, .threeOfKind]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testTwoPairKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        AATT3 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [.twoPair]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testOnePairKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        AA234 0
+        JA234 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [.onePair, .onePair]
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testHighCardKindJokersWild() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse("""
+        A2345 0
+        """)
+        let actual = game.hands.map {
+            let kind = Day07.Hand.kind(cards: $0.cards, jokersWild: true)
+            return kind
+        }
+        let expected: [Day07.Hand.Kind] = [.highCard]
+        XCTAssertNoDifference(expected, actual)
+    }
+
     func testOrderedHands() throws {
         let game = try Day07.GameParser().parse(testData)
         let actual = game.orderedHands()
         let expected = [0, 3, 2, 1, 4].map { game.hands[$0] }
+        XCTAssertNoDifference(expected, actual)
+    }
+
+    func testOrderedHandsP2() throws {
+        let game = try Day07.GameParser(jokersWild: true).parse(testData)
+        let actual = game.orderedHands()
+        let expected = [0, 2, 1, 4, 3].map { game.hands[$0] }
         XCTAssertNoDifference(expected, actual)
     }
 
@@ -58,7 +164,7 @@ final class Day07Tests: XCTestCase {
 
     func testPart2() async throws {
         let actual = try await Day07(data: testData).part2()
-        let expected = 0
+        let expected = 5905
         XCTAssertNoDifference(expected, actual)
     }
 }
